368096a78e70035789fb7c9a5f5d61cc7d593f03
mfarmalkhan <50073345+mfarmalkhan@users.noreply.github.com>
2020-12-21 16:36:39 +0500

    first commit

---
+++ app/src/main/java/com/authentik/ke/Instrument_List.java
@@ -0,0 +1,13 @@
+package com.authentik.ke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Instrument_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instrument__list);
+    }
+}
